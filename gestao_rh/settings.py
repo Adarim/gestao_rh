00eb83c9f4881834f1fba81020f86e15c035f924
Adarim <andregdarim@gmail.com>
2023-07-06 13:10:47 -0300

    debug

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = 'static/'
  #   os.path.join(BASE_DIR, "static"),
 #]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "gestao_rh/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
