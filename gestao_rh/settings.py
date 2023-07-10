ebdfab90e12e03f4211f2926b97ad89368c62b46
Adarim <andregdarim@gmail.com>
2023-07-10 15:33:25 -0300

    condensed statics

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -43,7 +43,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gestao_rh.urls'
@@ -112,7 +111,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_DIRS = [
  #   os.path.join(BASE_DIR, "static"),
 #]
