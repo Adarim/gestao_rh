bc3abf7f7deb638f9371a6e22e5e8a0ef43fbd5e
Adarim <andregdarim@gmail.com>
2023-07-13 12:38:41 -0300

    novo ip

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-i6#euop6l*jj6_25%u7vme0u=_=e-mfri_qw@%k!8=50t7x-5d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.222.118.80']
+ALLOWED_HOSTS = ['52.202.29.199']
 
 
 # Application definition
