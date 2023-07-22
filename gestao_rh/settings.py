094b0ffb864faedad225b93666556c08a55b639f
Adarim <andregdarim@gmail.com>
2023-07-22 15:51:18 -0300

    novas alterações 3

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
     'apps.core',
     'apps.empresas',
     'apps.funcionarios',
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
